653b012ee01632c0beba23885feeab839aacb5c5
Bradley Sheehan <bradpsheehan@gmail.com>
2013-09-04 13:27:21 -0400

    Add testimonials

--- assets/javascripts/animations.js
+++ assets/javascripts/animations.js
@@ -1,19 +1,17 @@
 $(document).ready(function (){
   $(".skills-button-fixed").click(function() {
-      // var duration = Math.abs($(this).scrollTop() - y) * 2; //2ms per pixel moved
       $('html, body').animate({
           scrollTop: $("#animation-target").offset().top
       }, 1500);
   });
 
+  $("#rec-button").click(function() {
+      $('html, body').animate({
+          scrollTop: $("#animation-target-2").offset().top
+      }, 1500);
+  });
 
-  // $('.circle').click(function(){
-  //   $(this).toggleClass('active')
-  // });
-
-  // $('.circle').click(function(){
-  //   console.log($(document).find(this));
-  // });
+  $('.skills-button').hide();
 
   $('#skills-content-left-top').toggle();
   $('#skills-content-left-bottom').toggle();
@@ -21,37 +19,23 @@ $(document).ready(function (){
   $('#skills-content-right-bottom').toggle();
   $('#skills-content-right-top').toggle();
 
-  // $('#skills-wrapper-place').click(function(){
-  //   $('#skills-content-left-top').toggle();
-  //   $('#skills-content-left-bottom').toggle();
-  //   $('#skills-content-bottom').toggle();
-  //   $('#skills-content-right-bottom').toggle();
-  //   $('#skills-content-right-top').toggle();
-  //   $('.circle').toggleClass('active');
-  // });
-
   $('#skills1').click(function(){
-    $(this).find('.circle').toggleClass('active');
     $('#skills-content-left-top').toggle();
   });
 
   $('#skills2').click(function(){
-    $(this).find('.circle').toggleClass('active');
     $('#skills-content-left-bottom').toggle();
   });
 
   $('#skills3').click(function(){
-    $(this).find('.circle').toggleClass('active');
     $('#skills-content-bottom').toggle();
   });
 
   $('#skills4').click(function(){
-    $(this).find('.circle').toggleClass('active');
     $('#skills-content-right-bottom').toggle();
   });
 
   $('#skills5').click(function(){
-    $(this).find('.circle').toggleClass('active');
     $('#skills-content-right-top').toggle();
 
   });
@@ -59,13 +43,13 @@ $(document).ready(function (){
   $(window).scroll(function() {
     var scroll = $(window).scrollTop();
     if (scroll > 700) {
-      // $('#skills-wrapper').attr('id', 'skills-wrapper-place');
       $('#skills1').addClass('transition');
       $('#skills2').addClass('transition-2');
       $('#skills3').addClass('transition-3');
       $('#skills4').addClass('transition-4');
       $('#skills5').addClass('transition-5');
       $('.skills-button-fixed').hide();
+      $('.skills-button').show();
     }
   });
 });
